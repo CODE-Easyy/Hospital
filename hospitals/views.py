57b81827f3ffc746ed11339e3e850c01f34d12b2
ALMAZ <61078111+Almazisheee@users.noreply.github.com>
2020-08-05 17:38:52 +0600

    .

--- hospitals/views.py
+++ hospitals/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from .models import Hospital
+
+
+
+def home(request):
+  hospitals = Hospital.objects.all()
+  context = {'objects': hospitals}
+  return render(request, 'hospitals/home.html', context)
